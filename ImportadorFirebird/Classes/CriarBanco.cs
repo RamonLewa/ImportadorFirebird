82afb7510cd1ebeee54133df34c00246c6cabd1b
AlexBruno099 <alexbrunorr099@gmail.com>
2024-09-14 23:20:50 -0300

    feat: Adicionado novas funções de paralelismo e funcionalidades no progessbar

--- ImportadorFirebird/Classes/CriarBanco.cs
+++ ImportadorFirebird/Classes/CriarBanco.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using FirebirdSql.Data.FirebirdClient;
 using System.IO;
 using System.Threading.Tasks;
-using FirebirdSql.Data.FirebirdClient;
 using System.Windows.Forms;
 
 namespace ImportadorFirebird.Classes
 {
     public class CriarBanco
     {
-        public async Task CreateDatabaseAsync()
+        public async Task CreateDatabaseAsync(ProgressBar progressBar)
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Banco de dados (*.FDB)|*.FDB";
-            saveFileDialog.Title = "Caminho banco de dados";
-            saveFileDialog.FileName = "BASESGMASTER50.FDB";
-            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Banco de dados (*.FDB)|*.FDB",
+                Title = "Caminho banco de dados",
+                FileName = "BASESGMASTER50.FDB",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
@@ -31,34 +27,33 @@ namespace ImportadorFirebird.Classes
                 {
                     if (File.Exists(caminhoArquivo))
                     {
-                        DialogResult dialogResult = MessageBox.Show("O arquivo já existe. Deseja sobrescrevê-lo?", "Sobrescrever", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-                        if (dialogResult == DialogResult.No)
-                        {
-                            return;
-                        }
-                        else
-                        {
-                            File.Delete(caminhoArquivo);
-                        }
+                        File.Delete(caminhoArquivo);
                     }
+
+                    FbConnectionStringBuilder builder = new FbConnectionStringBuilder
+                    {
+                        DataSource = "localhost",
+                        Database = caminhoArquivo,
+                        Port = 3051,
+                        UserID = "SYSDBA",
+                        Password = "masterkey",
+                        Charset = "UTF8",
+                        Dialect = 3,
+                        ConnectionLifeTime = 15,
+                        PacketSize = 8192,
+                        ServerType = 0,
+                        MaxPoolSize = 1000
+                    };
 
                     await Task.Run(() =>
                     {
-                        FbConnectionStringBuilder builder = new FbConnectionStringBuilder();
-                        builder.DataSource = "localhost";
-                        builder.Database = caminhoArquivo;
-                        builder.Port = 3051;
-                        builder.UserID = "SYSDBA";
-                        builder.Password = "masterkey";
-                        builder.Charset = "UTF8";
-                        builder.Dialect = 3;
-                        builder.ConnectionLifeTime = 15;
-                        builder.PacketSize = 8192;
-                        builder.ServerType = 0;
-                        builder.MaxPoolSize = 1000;
-
                         FbConnection.CreateDatabase(builder.ConnectionString);
                     });
+
+                    if (progressBar != null)
+                    {
+                        progressBar.Invoke(new Action(() => progressBar.Value = 100));
+                    }
 
                     MessageBox.Show("Banco de dados gerado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
